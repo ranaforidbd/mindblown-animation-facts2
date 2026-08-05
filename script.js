ffc6fce52f05f8f23283fa8aa51effffb11d5fc8
ranaforidbd <bdranaforid@gmail.com>
2026-08-05 20:47:19 +0600

    Update script.js

--- script.js
+++ script.js
@@ -7,59 +7,30 @@ text:"Mickey Mouse first appeared in 1928 in Steamboat Willie."
 },
 
 {
-title:"❄ Frozen",
+title:"❄️ Frozen",
 category:"Disney",
 image:"https://picsum.photos/400/250?random=2",
 text:"Frozen became one of Disney's biggest animated movies."
 },
 
 {
-title:"🎥 Toy Story",
+title:"🤠 Toy Story",
 category:"Pixar",
 image:"https://picsum.photos/400/250?random=3",
-text:"Toy Story was the world's first fully computer animated movie."
+text:"Toy Story was the world's first fully CGI feature film."
 },
 
 {
 title:"🧽 SpongeBob",
-category:"Cartoon",
+category:"Nickelodeon",
 image:"https://picsum.photos/400/250?random=4",
-text:"The creator of SpongeBob was a marine biology teacher."
+text:"SpongeBob's creator was a marine biology teacher."
 },
 
 {
-title:"🐼 Kung Fu Panda",
-category:"DreamWorks",
+title:"🦁 The Lion King",
+category:"Disney",
 image:"https://picsum.photos/400/250?random=5",
-text:"Po's facial expressions required many animators."
-},
-
-{
-title:"⚡ Naruto",
-category:"Anime",
-image:"https://picsum.photos/400/250?random=6",
-text:"Naruto has over 700 manga chapters."
+text:"The Lion King was inspired by Shakespeare's Hamlet."
 }
 ];
-
-const container=document.getElementById("factsContainer");
-
-facts.forEach(fact=>{
-
-container.innerHTML+=`
-
-<div class="card">
-
-<img src="${fact.image}" width="100%">
-
-<h3>${fact.title}</h3>
-
-<p>${fact.text}</p>
-
-<small>${fact.category}</small>
-
-</div>
-
-`;
-
-});
